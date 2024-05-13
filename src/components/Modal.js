bbc2c302ba2394cdd96145c2fdc08e87d967aeb7
nayiv <nayive432@gmail.com>
2024-05-12 22:32:31 -0500

    Modal updated

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,6 @@
 import React, { useEffect } from 'react';
 
 const Modal = ({ largeImageURL, alt, onClose }) => {
-  const handleKeyDown = (e) => {
-    if (e.key === 'Escape') {
-      onClose();
-    }
-  };
-
   const handleOverlayClick = (e) => {
     if (e.target === e.currentTarget) {
       onClose();
@@ -14,11 +8,17 @@ const Modal = ({ largeImageURL, alt, onClose }) => {
   };
 
   useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+
     window.addEventListener('keydown', handleKeyDown);
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, [handleKeyDown]);
+  }, [onClose]);
 
   return (
     <div className="overlay" onClick={handleOverlayClick}>
